72e8425c67bcea9ffe1df0f21f6558d5d7891b7a
ryoy0420 <biz.ryo.ai@gmail.com>
2025-02-05 07:10:26 +0900

    first commit

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "9d49c58a-35f0-4c25-b238-b661c724cf5a");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
